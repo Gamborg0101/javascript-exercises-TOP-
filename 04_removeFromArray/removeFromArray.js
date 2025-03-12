62b257f2790f534c9f564a66b3ba0380052bb227
Casper Gamborg <Gamborgc@gmail.com>
2025-03-12 07:38:43 +0100

    Did not think of saving this. But here we are

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, fjern) {
+
 };
+
+removeFromArray([1, 2, 3, 4], 3);
+
+/*
+1) array + fjern
+2) Iterer igennem alle elementerne 
+3) Hvis array == fjern brug med splice() 
+4) Returner resuiltat 
+
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
