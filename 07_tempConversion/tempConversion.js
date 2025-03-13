835b2ad832fdb1bc0e1f92f609e4fa819eb6879c
Casper Gamborg <Gamborgc@gmail.com>
2025-03-13 09:54:19 +0100

    TempConversion is done.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  return Math.round((fahrenheitTemp - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celciusTemp) {
+  return Math.round((celciusTemp * (9 / 5) + 32) * 10) / 10;
 };
+
+console.log(convertToCelsius(100)); // fahrenheit to celsius, should return 0.
+
+console.log(convertToFahrenheit(0)); // celsius to fahrenheit, should return 32
+
+/*
+We want to just return 1 decimal:
+37.1 for example.
+Use Math.round(num * 10) / 10
+
+*/
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
